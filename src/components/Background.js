7c892bae96dc623daf1590a14643389aba62a3f2
akshitrawat123 <63223269+akshitrawat123@users.noreply.github.com>
2023-05-31 16:50:48 +0530

    Add files via upload

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,29 +1,20 @@
 import React from "react";
-import Confirmation from "./Confirmation";
+import zIndex from "@mui/material/styles/zIndex";
 
 const Background = () => {
   return (
     <>
-    <div
-      style={{
-        position: "fixed",
-
-        zIndex: "50",
-
-        backgroundColor: "#000000",
-
-        opacity: "0.75",
-
-        width: "100%",
-
-        height: "100vh",
-
-        top: "0",
-
-        top: "0",
-      }}
-    />
-    <Confirmation/>
+      <div
+        style={{
+          position: "fixed",
+          zIndex: "50",
+          backgroundColor: "#000000",
+          opacity: "0.75",
+          width: "100%",
+          height: "100vh",
+          top: "0",
+        }}
+      />
     </>
   );
 };
